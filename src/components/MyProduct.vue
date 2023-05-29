94f3d11d4b0f83b1b9e554962495f735f89fa661
RaitoOrange <2977277015@qq.com>
2023-05-29 14:12:07 +0800

    提交

---
+++ src/components/MyProduct.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="product" @click="$emit('myclick', product.id)">
+    <img :src="product.src" alt="">
+    <div class="product-text">
+      <span>{{ product.name }}</span>
+      <span class="product-price">{{ product.price }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Myproduct',
+  props: ['product'],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.product {
+  border: 1px solid #ccc;
+  display: inline-block;
+  margin: 10px;
+}
+
+.product-text {
+  border-top: 1px solid #ccc;
+  padding: 10px;
+}
+
+.product-price {
+  float: right;
+}</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <MyProduct msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MyProduct from '@/components/MyProduct.vue'
+
+export default {
+  name: 'MyProduct',
+  components: {
+    MyProduct
+  }
+}
+</script>
