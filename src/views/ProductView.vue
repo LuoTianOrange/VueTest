e9c5f5bfc0df6cd9c7f5e9aeaef3e81a95f7629e
QingXia-Ela <Elahere@163.com>
2023-05-30 16:57:03 +0800

    feat: add product view sample

---
+++ src/views/ProductView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="product">
+    <MyProduct v-for="product in products" :key="product.id" :product="product" />
+  </div>
+</template>
+
+<script>
+import MyProduct from '@/components/MyProduct.vue';
+export default {
+  name: 'ProductView',
+  components: {
+    MyProduct
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: 'Product 1',
+          src: 'https://via.placeholder.com/150',
+          price: '$100'
+        },
+        {
+          id: 2,
+          name: 'Product 2',
+          src: 'https://via.placeholder.com/150',
+          price: '$200'
+        }
+      ]
+    }
+  }
+}
+</script>
